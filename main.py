40fea31dbfaf1898621263fae7013d9b88c366af
tsgm1257 <neaz1257@gmail.com>
2025-02-16 10:36:40 -0500

    Added matplotlib. Converted cost to a numpy array. Created graphs and show them.

--- main.py
+++ main.py
@@ -4,25 +4,43 @@
 
 import pprint
 import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
 
 mytickers = ["MSFT", "AAPL", "NVDA", "GME", "AMC"]
-
-mydata = {}
 
 mytickers.sort()
 for ticker in mytickers:
     dat = yf.Ticker(ticker)
-    mydata[ticker] = {'ticker': ticker,
-                      'dayHigh': dat.info["dayHigh"]
-                      }
-    pprint.pprint(mydata)
-
-# dat = yf.Ticker("AAPL")
+    hist = dat.history(period="10d")
+    # pprint.pprint(dat.info)
+    last10days = []
+    for date in hist['Close'][:11]:
+        last10days.append(date)
+    if len(last10days) == 10:
+        # maxlist = copy.copy(last10days)
+        # maxlist.sort()
+        # max_price = maxlist[-1]+10
+        # min_price = maxlist[0]-10
+        myarray = np.array(last10days)
+        max_price = myarray.max() + (myarray.max() * .05)
+        min_price = myarray.min() - (myarray.min() * .05)
+        plt.plot(myarray)
+        plt.title(f"{ticker} Last 10 Closing Prices")
+        plt.xlabel("Days Ago")
+        plt.ylabel("Closing Price")
+        plt.axis((9, 0, min_price, max_price))
+        plt.show()
+    else:
+        print("Do not have 10 days data. Only have {len(last10days)} days data.")
+        # pprint.pprint(myarray)
+    # pprint.pprint(hist['Close'])
 
 # get all stock info
-# pprint.pprint(dat.info)
+
 
 # get historical market date
 # hist = dat.history(period="10d")
 #
-# pprint.pprint(hist)
+#
